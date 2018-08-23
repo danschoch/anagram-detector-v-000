b7c097a248df3b2292277c1afc00f2efd02fba68
Daniel Schoch <d.schoch90@gmail.com>
2018-08-23 18:02:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,4 +4,14 @@ class Anagram
   def initialize(word)
     @word = word
   end
+
+  def match(list)
+    list.each do |comparison|
+      anagrams = []
+      comparison_arr = comparison.split("").sort
+      if comparison_arr == @word.split("").sort
+        anagrams << comparison
+      end
+    end
+  end
 end
